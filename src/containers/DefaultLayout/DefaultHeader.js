d27434a072440bc98b57790533fc87cbb3789991
Gabriel Oshiro <gabriel.oshiro@cbyk.com.br>
2020-06-30 20:30:00 -0300

    Ajuste botao logout

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -1,53 +1,60 @@
 import React, { Component } from 'react';
-import {  UncontrolledDropdown, DropdownMenu, DropdownToggle, Nav } from 'reactstrap';
+import { UncontrolledDropdown, DropdownMenu, DropdownToggle, Nav, DropdownItem } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
-import { Logout } from '../../app/redux/actions/Login/LoginActions'; 
-import { AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
+import { Logout } from '../../app/redux/actions/Login/LoginActions';
+import { AppSidebarToggler } from '@coreui/react';
 
 const propTypes = {
-  children: PropTypes.node,
+	children: PropTypes.node,
 };
 
 const defaultProps = {};
 
 class DefaultHeader extends Component {
 
-  logout(e) {
-    e.preventDefault();
-    this.props.Logout();
-    this.props.history.push('/Login');
-  }
-  render() {
+	logout(e) {
+		e.preventDefault();
+		this.props.Logout();
+		this.props.history.push('/Login');
+	}
+	render() {
 
-    // eslint-disable-next-line
-    const { children, ...attributes } = this.props;
+		// eslint-disable-next-line
+		const { children, ...attributes } = this.props;
 
-    return (
-      <React.Fragment>
-        <AppSidebarToggler className="d-lg-none" display="md" mobile />
-        <AppNavbarBrand
-        //   full={{ src: logo, width: 89, height: 25, alt: 'CoreUI Logo' }}
-        //   minimized={{ src: sygnet, width: 30, height: 30, alt: 'CoreUI Logo' }}
-        />
-        <AppSidebarToggler className="d-md-down-none" display="lg" />
-        <Nav className="ml-auto" navbar>
-          <UncontrolledDropdown nav direction="down">
-            <DropdownToggle className="pr-3" nav>
-			<Link   
-                to="/Login"
-                onClick={this.props.Logout}>
-				<i id="icon-logout" class="fa fa-sign-out fa-2x" aria-hidden="true" data-toggle="tooltip" data-placement="bottom" title="Sair"></i>
-            </Link>
-            </DropdownToggle>
-            <DropdownMenu right>
-            </DropdownMenu>
-          </UncontrolledDropdown>
-        </Nav>
-      </React.Fragment>
-    );
-  }
+		return (
+			<React.Fragment>
+				<AppSidebarToggler className="d-lg-none" display="md" mobile />
+				<AppSidebarToggler className="d-md-down-none" display="lg" />
+				<Nav className="ml-auto" navbar>
+					<UncontrolledDropdown nav direction="down">
+						<DropdownToggle nav>
+							<i class="fa fa-cog fa-lg" aria-hidden="true"></i>
+						</DropdownToggle>
+						<DropdownMenu right>
+							<DropdownItem header tag="div" className="text-center"><strong>Opções</strong></DropdownItem>
+							{/* <DropdownItem onClick={() => this.exibirModal()}><i class="fa fa-user-circle" aria-hidden="true"></i>Editar Perfil</DropdownItem> */}
+							<DropdownItem header tag="div" className="text-center align-middle">
+								<Link
+									id="btnSair"
+									to="/Login"
+									onClick={this.props.Logout}
+									style={{
+										'text-decoration': 'none',
+										'color': '#000000'
+									}}>
+									<i id="icon-logout" class="fa fa-sign-out fa-lg mr-2" aria-hidden="true"></i>
+								Sair
+								</Link>
+							</DropdownItem>
+						</DropdownMenu>
+					</UncontrolledDropdown>
+				</Nav>
+			</React.Fragment>
+		);
+	}
 }
 
 DefaultHeader.propTypes = propTypes;
